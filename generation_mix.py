324d7bd1b9e919ca6ad0af63fced219bec716cff
Adam Eldridge <adam@thinlamp.co.uk>
2018-08-24 14:20:02 +0000

    simplified json parsing

--- generation_mix.py
+++ generation_mix.py
@@ -19,23 +19,6 @@ def get_last_30_mins():
 
     # Parse data
     for i in json_data["data"]["generationmix"]:
-        if i["fuel"] == "biomass":
-            gen_mix["biomass"] = i["perc"]
-        elif i["fuel"] == "coal":
-            gen_mix["coal"] = i["perc"]
-        elif i["fuel"] == "imports":
-            gen_mix["imports"] = i["perc"]
-        elif i["fuel"] == "gas":
-            gen_mix["gas"] = i["perc"]
-        elif i["fuel"] == "nuclear":
-            gen_mix["nuclear"] = i["perc"]
-        elif i["fuel"] == "other":
-            gen_mix["other"] = i["perc"]
-        elif i["fuel"] == "hydro":
-            gen_mix["hydro"] = i["perc"]
-        elif i["fuel"] == "solar":
-            gen_mix["solar"] = i["perc"]
-        elif i["fuel"] == "wind":
-            gen_mix["wind"] = i["perc"]
+        gen_mix[i["fuel"]] = i["perc"]
 
     return gen_mix
