02255a4a3c9c75f01390bc1c5139ceec2d4e2f93
Adam Eldridge <adam@thinlamp.co.uk>
2018-08-28 08:54:32 +0100

    Fixed merge conflict in generation_mix

---
+++ generation_mix.py
@@ -0,0 +1,41 @@
+# Import dependencies
+import requests
+
+# Set up common vars
+base_url = "https://api.carbonintensity.org.uk/generation"
+
+# Set up fuel types
+fuel_types = {
+    'biomass': 'dirty',
+    'coal': 'dirty',
+    'gas': 'dirty',
+    'hydro': 'clean',
+    'imports': 'dirty',
+    'nuclear': 'dirty',
+    'other': 'dirty',
+    'solar': 'clean',
+    'wind': 'clean'}
+
+
+# Get the gen mix for all fuels from the last 30 mins
+def get_last_30_mins(fuel_type):
+
+    # Get raw data
+    headers = {'Accept': 'application/json'}
+    json_data = requests.get(
+        base_url,
+        params={},
+        headers=headers).json()
+
+    gen_mix = {}
+
+    # Get percentages for fuels based on fuel type
+    for i in json_data["data"]["generationmix"]:
+
+        if fuel_type == 'all':
+            gen_mix[i["fuel"]] = i["perc"]
+        else:
+            if fuel_types[i["fuel"]] == fuel_type:
+                gen_mix[i["fuel"]] = i["perc"]
+
+    return gen_mix
--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # Test getting data from gen mix and pass to pie chart
-gen_mix_pie_chart = pie_chart.parts(generation_mix.get_last_30_mins("dirty"))
+gen_mix_pie_chart = pie_chart.parts(generation_mix.get_last_30_mins("clean"))
 pp.pprint(gen_mix_pie_chart)
