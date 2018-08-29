634b42c44086ead370cab3bff077b0f531251006
Adam Eldridge <adam@thinlamp.co.uk>
2018-08-29 23:11:27 +0000

    fixed error handling

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ gen_mix_last_30_mins = generation_mix.get_last_30_mins("clean")
 gen_mix_pie_chart = pie_chart.calc_parts(gen_mix_last_30_mins, "individual")
 
 pp.pprint(gen_mix_last_30_mins)
-#pp.pprint(gen_mix_pie_chart)
+pp.pprint(gen_mix_pie_chart)
